5ab92afe3927c8a66323cd72d9d2f7f5b6cec2c8
Максим <gmax7911@gmail.com>
2023-12-23 11:37:19 +0300

    The "Task" abstract class has been created

--- src/task/Task.java
+++ src/task/Task.java
@@ -3,9 +3,12 @@ package task;
 import task.type.Type;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 
-/** Класс "Задача" */
+/**
+ * Класс "Задача"
+ */
 
 public abstract class Task {
     private static int idGenerator = 0;
@@ -21,6 +24,58 @@ public abstract class Task {
         this.description = description;
         this.id = ++idGenerator;
         this.dateTime = LocalDateTime.now();
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Task task)) return false;
+        return id == task.id && Objects.equals(title, task.title) && type == task.type && Objects.equals(dateTime, task.dateTime) && Objects.equals(description, task.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, type, id, dateTime, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "title='" + title + '\'' +
+                ", type=" + type +
+                ", id=" + id +
+                ", dateTime=" + dateTime +
+                ", description='" + description + '\'' +
+                '}';
     }
 }
 
